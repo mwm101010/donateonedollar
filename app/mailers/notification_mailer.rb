ffa1b7a20fab05305839a1c765515a2a2023725f
herangakr <ma.muthukuda@gmail.com>
2019-10-31 02:06:37 +0000

    Added auto generates a mail to admin once the user submits the contact us form

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@donateonedollar.com"
 
-  def message_added
+  def message_added(contact)
+    @message = contact.message
+    @name = contact.name
     mail(to: "mamw.muthu@gmail.com",
-         subject: "A new message has been added to database")
+         subject: "A new message has been added to the database by #{@name}")
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,14 @@
 class Contact < ApplicationRecord
+  after_create :send_message_email
  
- validates :name, presence: true
- validates :email, presence: true
- validates :phone_number, presence: true
- validates :message, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :message, presence: true
 
  
   def send_message_email
-      NotificationMailer.message_added(self).deliver_now
+      NotificationMailer.message_added.deliver_now
   end
 
 end
